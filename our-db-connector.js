79841b3aa99c685262a72625ef59ad3ee6ff0d27
John Tomaselli <jrt@gtz.com>
2022-04-02 19:39:00 -0400

    Update our-db-connector.js

--- our-db-connector.js
+++ our-db-connector.js
@@ -1,13 +1,14 @@
 // ESM
 import fastifyPlugin from 'fastify-plugin'
 import fastifyMongo from 'fastify-mongodb'
-
+// const MONGODB_URLtodo='mongodb://127.0.0.1:27017/todo' // this works fine
+// test with the next line fails
+const MONGODB_URLtodo='mongodb://admin:<Password>@SG-test-fastify-50496.servers.mongodirector.com:27017/todo?ssl=true&connectTimeoutMS=10000&authSource=admin&authSource=admin' // &authMechanism=SCRAM-SHA-1'
 async function dbConnector (fastify, options) {
   fastify.register(fastifyMongo, {
-    url: 'mongodb://localhost:27017/test_database'
+    
+    url:MONGODB_URLtodo
   })
 }
 
-// Wrapping a plugin function with fastify-plugin exposes the decorators
-// and hooks, declared inside the plugin to the parent scope.
 export default fastifyPlugin(dbConnector)
